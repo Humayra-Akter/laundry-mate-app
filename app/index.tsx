5e1c81eebfeedc2cc9d1ff76a200042015a61bff
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-07-08 23:29:11 +0600

    auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { Redirect } from "expo-router";
-import ProviderWrapper from "./ProviderWrapper";
 
 const Index = () => {
   return (
-    <ProviderWrapper>
       <Redirect href={"/(tabs)/home"} />
-    </ProviderWrapper>
   );
 };
 
